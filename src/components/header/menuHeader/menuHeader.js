e317ea555d556a8ac7f7f4c4769c26fd23c91b23
AntonniSMoraes <antonnimoraes@gmail.com>
2026-04-26 22:10:17 -0300

    feat: configuração menuHeader.js, homePage.js

--- src/components/header/menuHeader/menuHeader.js
+++ src/components/header/menuHeader/menuHeader.js
@@ -1,15 +1,35 @@
 export const MenuHeader = () => {
     return(
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>Sobre</li>
-                <li>Notícias</li>
-                <li>Programação</li>
-                <li>Horoscopo</li>
-                <li>Eventos</li>
-                <li>Contato</li>
+        <nav style={container}>
+            <ul style={menu}>
+                <li><a style={linkStyle} href="/">Home</a></li>
+                <li><a style={linkStyle} href="/sobre">Sobre</a></li>
+                <li><a style={linkStyle} href="/noticias">Notícias</a></li>
+                <li><a style={linkStyle} href="/programacao">Programação</a></li>
+                <li><a style={linkStyle} href="/horoscopo">Horoscopo</a></li>
+                <li><a style={linkStyle} href="/eventos">Eventos</a></li>
+                <li><a style={linkStyle} href="/contato">Contato</a></li>
             </ul>
         </nav>
     )
+}
+
+const container = {
+    display: "flex",
+    width : "100%",
+    padding: "10px",
+    justifyContent: "center",
+    backgroundColor: "rgb(121, 121, 121)",
+}
+
+const menu = {
+    display: 'flex',
+    width: '50%',
+    listStyle: 'none',
+}
+
+const linkStyle = {
+    textDecoration: 'none',
+    color: '#fff',
+    fontWeight: 'bold',
 }
